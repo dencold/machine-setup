b88452da50678924ae6f1112b428755e81c609ee
Dennis Coldwell <dennis.coldwell@gmail.com>
2015-12-15 23:37:59 -0800

    adding machine setup for linux (arch)

---
+++ linux/arch-install-core.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+# ---------------------------------------------------------------------------------------
+#
+# Author: Dennis Coldwell (@dencold)
+#
+
+# Ask for the administrator password upfront.
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until the script has finished.
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+# the go programming language
+pacman --noconfirm -S go
+
+# node and npm
+pacman --noconfirm -S nodejs npm
+
