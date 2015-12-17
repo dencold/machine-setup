ada19635f89ffcc6ace0fe9c4d18893070ed142f
Dennis Coldwell <dennis.coldwell@gmail.com>
2015-12-16 22:48:42 -0800

    adding apps installation script

---
+++ linux/arch-install-apps.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# ---------------------------------------------------------------------------------------
+#
+# Author: Dennis Coldwell (@dencold)
+#
+
+# make sure that arch is fully updated
+pacman --noconfirm -Syu
+
+# anki spaced repetition learning program
+pacman --noconfirm -S anki
+
