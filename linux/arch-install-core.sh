f29e52420388db5340ba68fe13c2afea9290212f
Dennis Coldwell <dennis.coldwell@gmail.com>
2015-12-16 22:49:55 -0800

    adding system update check, removing sudo in script

--- linux/arch-install-core.sh
+++ linux/arch-install-core.sh
@@ -4,11 +4,8 @@
 # Author: Dennis Coldwell (@dencold)
 #
 
-# Ask for the administrator password upfront.
-sudo -v
-
-# Keep-alive: update existing `sudo` time stamp until the script has finished.
-while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+# ensure that arch is up to date
+pacman --noconfirm -Syu
 
 # the go programming language
 pacman --noconfirm -S go
