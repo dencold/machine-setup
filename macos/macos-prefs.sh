ae5bd6cfba1f332ac0c89da92dbaf6863fc2319b
Dennis Coldwell <dennis.coldwell@gmail.com>
2017-01-20 12:43:19 -0800

    removing dupes in prefs setting

--- macos/macos-prefs.sh
+++ macos/macos-prefs.sh
@@ -98,9 +98,7 @@ defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
 
 # Disable auto-substitutions
 defaults write NSGlobalDomain NSAutomaticCapitalizationEnabled -bool false
-defaults write NSGlobalDomain NSAutomaticDashSubstitutionEnabled -bool false
 defaults write NSGlobalDomain NSAutomaticPeriodSubstitutionEnabled -bool false
-defaults write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool false
 defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
 
 ###############################################################################
