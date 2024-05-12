92389e314ce3ec4815e44f4b5514c33334951db5
Dennis Coldwell <dennis.coldwell@gmail.com>
2024-05-12 14:32:27 -0700

    removing bash references from homebrew script, we use zsh as default now

--- macos/homebrew.sh
+++ macos/homebrew.sh
@@ -37,19 +37,6 @@ brew install moreutils
 # Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
 brew install findutils
 
-# Install Bash 4.
-# Note: don’t forget to add `/usr/local/bin/bash` to `/etc/shells` before
-# running `chsh`.
-brew install bash
-brew tap homebrew/versions
-brew install bash-completion2
-
-# Switch to using brew-installed bash as default shell
-if ! fgrep -q '/usr/local/bin/bash' /etc/shells; then
-  echo '/usr/local/bin/bash' | sudo tee -a /etc/shells;
-  chsh -s /usr/local/bin/bash;
-fi;
-
 # Install wget
 brew install wget
 
@@ -57,7 +44,7 @@ brew install wget
 brew install lua
 
 # finally, a vim install that isn't 5 years old.
-brew install vim --with-lua -with-override-system-vi
+brew install vim --with-lua -with-override-system-v
 
 # Install more recent versions of some macOS tools.
 brew install homebrew/dupes/grep
